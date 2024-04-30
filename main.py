b090c2c2e55e0f5ad44936f061c0075fea3e2199
Brandon Minjares <bmminjar@ucsc.edu>
2024-04-30 16:25:19 -0700

    added some start logic

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
+import random
 from board import Board
 from Pieces.pawn import Pawn
+from player import Player
 
 def main():
 	game = Board()
 	game.initializeBoard()
-	# game.printBoard()
-	pawn = game.board[1][0]
-	print(pawn.allowableMoves())
+
+	# chooses who is black and white
+	num = random.randint(0, 1)
+
+	playerOne = Player("Bran", "white")
+	playerTwo = Player("Adam", "black")
+
+	while(1):
+		if playerOne.playerTurn == 1:
+			print("Player one turn")
+			playerOne.playerTurn = 0
+			playerTwo.playerTurn = 1
+		else:
+			print("Player two turn")
+			playerTwo.playerTurn = 0
+			playerOne.playerTurn == 1
+
 	
-	"""
-	game.board[3][0] = pawn
-	game.board[1][0] = "-"
-	game.printBoard()
-	"""
+
 
 if __name__ == "__main__":
 	main()
--- player.py
+++ player.py
@@ -1,8 +1,8 @@
 class Player:
-    def __init__(self, name, color, points):
+    def __init__(self, name, color):
         self.name = name
         self.color = color
-        self.points = points
+        self.points = 0
 
         # White plays first, then alternates
         self.playerTurn = 1 if self.color == 'white' else 0
